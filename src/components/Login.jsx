a4b80f26ffa223aa2318f804576cea9ec4e3524e
YoussefElogail <125933702+YoussefElogail@users.noreply.github.com>
2023-11-01 22:32:05 +0200

    Build Login component

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,8 +1,49 @@
+import { Button, Card, Form } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
 const Login = () => {
   return (
-    <div>Login</div>
-  )
-}
+    <>
+      <Card>
+        <Card.Body>
+          <h2 className="text-center mb-4">Log In</h2>
 
-export default Login
+          <Form className="d-flex flex-column gap-4">
+            <Form.Group>
+              <Form.Label htmlFor="email">Email:</Form.Label>
+              <Form.Control
+                type="email"
+                id="email"
+                placeholder="Enter your email"
+              />
+            </Form.Group>
+
+            <Form.Group>
+              <Form.Label htmlFor="password">Password:</Form.Label>
+              <Form.Control
+                type="password"
+                id="password"
+                placeholder="Enter your password"
+              />
+            </Form.Group>
+
+            <Button variant="primary" type="submit" className="w-100">
+              Log In
+            </Button>
+          </Form>
+          <div className="w-100 text-center mt-4">
+            <Link to="/forgot-password">
+              Forgot your password?
+            </Link>
+          </div>
+        </Card.Body>
+      </Card>
+
+      <div className="w-100 text-center mt-2">
+        {"Don't have an account?"} <Link to="/signup">Signup</Link>
+      </div>
+    </>
+  );
+};
+
+export default Login;
